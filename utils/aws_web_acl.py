6bd9822b8e6fe9a0b0bd53b1a0ddbf85a6bc85ed
Venkata Mutyala <venkata@venkatamutyala.com>
2023-10-09 23:14:13 +0000

    fixes

--- utils/aws_web_acl.py
+++ utils/aws_web_acl.py
@@ -8,14 +8,12 @@ def get_webacl_arn_from_name(name, scope='CLOUDFRONT'):
     # Create WAFv2 client
     waf = create_aws_client('wafv2')
 
-    # Initialize pagination
-    paginator = waf.get_paginator('list_web_acls')
-    iterator = paginator.paginate(Scope=scope)
+    # Call list_web_acls to retrieve the list of WebACLs
+    response = waf.list_web_acls(Scope=scope)
 
     # Loop through the WebACLs and match by name
-    for page in iterator:
-        for web_acl in page['WebACLs']:
-            if web_acl['Name'] == name:
-                return web_acl['ARN']
+    for web_acl in response['WebACLs']:
+        if web_acl['Name'] == name:
+            return web_acl['ARN']
             
     raise Exception(f"Unable to find Web ACL with name: {name}")
