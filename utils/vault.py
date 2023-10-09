5ca917bde0c75863e6f465c8640f7c36356b0cfa
Venkata Mutyala <venkata@venkatamutyala.com>
2023-10-09 19:30:35 +0000

    fixes

--- utils/vault.py
+++ utils/vault.py
@@ -51,7 +51,8 @@ def get_data_from_vault(secret_path):
     if response.headers.get('Location'):
         raise HTTPException(status_code=400, detail=f"We got a redirect response when trying to read a secret from Vault. You are probably using pomerium or something went wrong in cluster and your token expired.")
 
-    response_data = response.json().get('data').get('data')
     if not response_data:
-        raise HTTPException(status_code=404, detail=f"Failed to retrieve certificate data from Vault.")
+        raise HTTPException(status_code=404, detail=f"Failed to retrieve certificate data from Secret Store.")
+    response_data = response.json().get('data').get('data')
+
     return response_data
