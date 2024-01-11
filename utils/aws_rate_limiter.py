2f97d38c6cde730fd5e90b1e6c3cddc5d48b45b7
Venkata Mutyala <venkata.mutyala@glueops.dev>
2024-01-11 23:28:07 +0000

    initial test

---
+++ utils/aws_rate_limiter.py
@@ -0,0 +1,35 @@
+from pyrate_limiter import Duration, Rate, Limiter, BucketFullException, RedisBucket
+from redis import Redis
+import os
+
+REDIS_CONNECTION = os.environ.get('REDIS_CONNECTION_STRING', 'redis://glueops-operator-shared-redis.glueops-core-operators.svc.cluster.local:6379')
+
+class RateLimiterUtil:
+    def __init__(self, redis_url=REDIS_CONNECTION):
+        self.redis_client = Redis.from_url(redis_url)
+
+        # Initialize AWS ACM limiters
+        self.aws_acm_describe_certificate_limiter = self.create_limiter("ratelimit:aws:acm:describecertificate", 10, Duration.SECOND)
+        self.aws_acm_import_certificate_limiter = self.create_limiter("ratelimit:aws:acm:importcertificate", 1, Duration.SECOND)
+        self.aws_acm_request_certificate_limiter = self.create_limiter("ratelimit:aws:acm:requestcertificate", 5, Duration.SECOND)
+        self.aws_acm_delete_certificate_limiter = self.create_limiter("ratelimit:aws:acm:deletecertificate", 5, Duration.SECOND)
+
+    def try_acquire(self, limiter, item_key):
+        limiter.try_acquire(item_key, raise_when_fail=True, max_delay=8000)
+        return True
+
+    def create_limiter(self, key, rate, duration):
+        bucket = RedisBucket.init(Rate(rate, duration), self.redis_client, key)
+        return Limiter(bucket)
+
+    def allow_request_aws_acm_describe_certificate(self):
+        return self.try_acquire(self.aws_acm_describe_certificate_limiter, "aws_acm_describe_certificate")
+        
+    def allow_request_aws_acm_import_certificate(self):
+        return self.try_acquire(self.aws_acm_import_certificate_limiter, "aws_acm_import_certificate")
+
+    def allow_request_aws_acm_request_certificate(self):
+        return self.try_acquire(self.aws_acm_request_certificate_limiter, "aws_acm_request_certificate")
+        
+    def allow_request_aws_acm_delete_certificate(self):
+        return self.try_acquire(self.aws_acm_delete_certificate_limiter, "aws_acm_delete_certificate")
