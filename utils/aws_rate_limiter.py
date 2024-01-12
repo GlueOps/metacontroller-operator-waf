e97e98954ad9e043a6e33e10af0507238d1ddd69
Venkata Mutyala <venkata.mutyala@glueops.dev>
2024-01-12 03:28:28 +0000

    tryuing again

--- utils/aws_rate_limiter.py
+++ utils/aws_rate_limiter.py
@@ -7,7 +7,11 @@ import os
 
 logger = glueops.setup_logging.configure(level=os.environ.get('LOG_LEVEL', 'INFO'))
 
-
+BUCKET_PREFIX="ratelimit:aws:acm:"
+AWS_ACM_DESCRIBE_CERT="aws_acm_describe_certificate"
+AWS_ACM_IMPORT_CERT="aws_acm_import_certificate"
+AWS_ACM_REQUEST_CERT="aws_acm_request_certificate"
+AWS_ACM_DELETE_CERT="aws_acm_delete_certificate"
 
 
 class RateLimiterUtil:
@@ -15,15 +19,15 @@ class RateLimiterUtil:
         self.redis_client = Redis(connection_pool=ConnectionPool.from_url(redis_url))
 
         # Initialize AWS ACM limiters
-        self.aws_acm_describe_certificate_limiter = self.create_limiter("ratelimit:aws:acm:describecertificate", 10, Duration.SECOND)
-        self.aws_acm_import_certificate_limiter = self.create_limiter("ratelimit:aws:acm:importcertificate", 1, Duration.SECOND)
-        self.aws_acm_request_certificate_limiter = self.create_limiter("ratelimit:aws:acm:requestcertificate", 5, Duration.SECOND)
-        self.aws_acm_delete_certificate_limiter = self.create_limiter("ratelimit:aws:acm:deletecertificate", 5, Duration.SECOND)
+        self.aws_acm_describe_certificate_limiter = self.create_limiter(f"{BUCKET_PREFIX}{AWS_ACM_DESCRIBE_CERT}", 10, Duration.SECOND)
+        self.aws_acm_import_certificate_limiter = self.create_limiter(f"{BUCKET_PREFIX}{AWS_ACM_IMPORT_CERT}", 1, Duration.SECOND)
+        self.aws_acm_request_certificate_limiter = self.create_limiter(f"{BUCKET_PREFIX}{AWS_ACM_REQUEST_CERT}", 5, Duration.SECOND)
+        self.aws_acm_delete_certificate_limiter = self.create_limiter(f"{BUCKET_PREFIX}{AWS_ACM_DELETE_CERT}", 5, Duration.SECOND)
 
     def check(self, limiter, item_key):
         try:
             logger.info(f"Checking rate limit for: {item_key} ")
-            limiter.delay_or_raise(item=item_key)
+            limiter.delay_or_raise(bucket=f"{BUCKET_PREFIX}{item_key}",item=item_key)
             return True
         except BucketFullException as err:
             logger.error(err)
@@ -39,13 +43,13 @@ class RateLimiterUtil:
         return Limiter(bucket, raise_when_fail=True, max_delay=120000) # 30s
 
     def allow_request_aws_acm_describe_certificate(self):
-        return self.check(self.aws_acm_describe_certificate_limiter, "aws_acm_describe_certificate")
+        return self.check(limiter=self.aws_acm_describe_certificate_limiter, item_key=AWS_ACM_DESCRIBE_CERT)
         
     def allow_request_aws_acm_import_certificate(self):
-        return self.check(self.aws_acm_import_certificate_limiter, "aws_acm_import_certificate")
+        return self.check(limiter=self.aws_acm_import_certificate_limiter, item_key=AWS_ACM_IMPORT_CERT)
 
     def allow_request_aws_acm_request_certificate(self):
-        return self.check(self.aws_acm_request_certificate_limiter, "aws_acm_request_certificate")
+        return self.check(limiter=self.aws_acm_request_certificate_limiter, item_key=AWS_ACM_REQUEST_CERT)
         
     def allow_request_aws_acm_delete_certificate(self):
-        return self.check(self.aws_acm_delete_certificate_limiter, "aws_acm_delete_certificate")
+        return self.check(limiter=self.aws_acm_delete_certificate_limiter, item_key=AWS_ACM_DELETE_CERT)
